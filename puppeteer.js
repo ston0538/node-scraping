0999c9fcd8c323ae41b9e82b0fd37feb2ae850c3
hk <ston0538@gmail.com>
2021-05-26 14:31:06 +0900

    feat: goto url 변경

--- puppeteer.js
+++ puppeteer.js
@@ -5,15 +5,15 @@ async function app() {
   console.log(description);
 }
 
-async function getDescription(symbol) {
+async function getDescription() {
   try {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     // await page.goto("http://ih.advfn.com/stock-market/NASDAQ/TSLA/stock-price");
     // await page.goto("https://www.airbnb.co.kr/d/howairbnbworks");
-    await page.goto("https://seouldonuts.co.kr/");
+    await page.goto("https://pxd-fed-blog.web.app/");
     const text = await page.evaluate(() => {
-      return document.querySelector(".hide.css-1mhthxx").innerText;
+      return document.querySelector(".post-right").innerText;
     });
     await browser.close();
     return text;
